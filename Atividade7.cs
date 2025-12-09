733d92e9c4f288ca305b1a69e53144e1eaaab7fd
João Maik <maikalmeida2406@gmail.com>
2025-12-09 20:21:38 -0300

    Create Atividade7.cs

---
+++ Atividade7.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+class TabelaHash
+{
+    private List<string>[] tabela = new List<string>[5];
+
+    public TabelaHash()
+    {
+        for (int i = 0; i < tabela.Length; i++)
+            tabela[i] = new List<string>();
+    }
+
+    private int FuncaoHash(string chave)
+    {
+        return chave.Length % 5;
+    }
+
+    public void Adicionar(string palavra)
+    {
+        int indice = FuncaoHash(palavra);
+        tabela[indice].Add(palavra);
+        Console.WriteLine($"Palavra '{palavra}' armazenada no índice {indice}.");
+    }
+
+    public void ExibirTabela()
+    {
+        Console.WriteLine("\n--- Estado da Tabela Hash ---");
+
+        for (int i = 0; i < tabela.Length; i++)
+        {
+            Console.Write($"Índice {i}: ");
+            foreach (var item in tabela[i])
+            {
+                Console.Write($"[{item}] -> ");
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TabelaHash dicionario = new TabelaHash();
+
+        dicionario.Adicionar("Cat");
+        dicionario.Adicionar("Dog");
+        dicionario.Adicionar("Bird");
+        dicionario.Adicionar("Ox");
+
+        dicionario.ExibirTabela();
+    }
+}
